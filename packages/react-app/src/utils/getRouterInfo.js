8bc5e2ebbf138ac9a203273ef9ccc153be48b074
Edward Anthony Escudero Bowie <57167218+Kayeddy@users.noreply.github.com>
2022-12-12 09:15:00 -0500

    Updated Contracts folder

--- packages/react-app/src/utils/getRouterInfo.js
+++ packages/react-app/src/utils/getRouterInfo.js
@@ -1,7 +1,7 @@
 import { abis } from "@my-app/contracts";
 
 export const getRouterInfo = async (routerAddress, web3) => {
-  const router = new web3.eth.Contract(abis.erc20.values, routerAddress);
+  const router = new web3.eth.Contract(abis.router02, routerAddress);
 
   return {
     factory: await router.methods.factory().call(),
